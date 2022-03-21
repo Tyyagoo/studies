8d2bfe8717fcf9855957c7f712c2c64e6c909de6
Tyyagoo <kinuchiha87@gmail.com>
2022-03-21 15:10:24 -0300

    :sparkles: add `rna transcription` exercise

---
+++ exercism/elixir/rna-transcription/lib/rna_transcription.ex
@@ -0,0 +1,19 @@
+defmodule RnaTranscription do
+  @dna2rna %{
+    ?G => ?C,
+    ?C => ?G,
+    ?T => ?A,
+    ?A => ?U
+  }
+
+  @doc """
+  Transcribes a character list representing DNA nucleotides to RNA
+
+  ## Examples
+
+  iex> RnaTranscription.to_rna('ACTG')
+  'UGAC'
+  """
+  @spec to_rna([char]) :: [char]
+  def to_rna(dna), do: Enum.map(dna, &Map.get(@dna2rna, &1))
+end
