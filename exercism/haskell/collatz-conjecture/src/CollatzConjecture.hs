f9606689de14ee570e37a5753f908c42e7efa090
Tyyagoo <tyyago.dev@gmail.com>
2023-02-13 11:50:44 -0300

    'do'

--- exercism/haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ exercism/haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -3,10 +3,10 @@ module CollatzConjecture (collatz) where
 collatz :: Integer -> Maybe Integer
 collatz n
   | n <= 0 = Nothing
-  | otherwise = doCollatz n 0
+  | otherwise = collatz' n 0
 
-doCollatz :: Integer -> Integer -> Maybe Integer
-doCollatz n steps
+collatz' :: Integer -> Integer -> Maybe Integer
+collatz' n steps
   | n == 1 = Just steps
-  | n `mod` 2 == 0 = doCollatz (n `div` 2) (steps + 1)
-  | otherwise = doCollatz (n * 3 + 1) (steps + 1)
+  | n `mod` 2 == 0 = collatz' (n `div` 2) (steps + 1)
+  | otherwise = collatz' (n * 3 + 1) (steps + 1)
