2b354bddee3836cea4fee07f65fffbd3e4ad6a4a
Tyyagoo <tyyago.dev@gmail.com>
2023-03-01 15:48:51 -0300

    :art: refactor `elixir/robot-simulator` exercise

--- exercism/elixir/robot-simulator/lib/robot_simulator.ex
+++ exercism/elixir/robot-simulator/lib/robot_simulator.ex
@@ -4,7 +4,12 @@ defmodule RobotSimulator do
   @directions [:north, :east, :south, :west]
 
   defguardp is_valid_direction(dir) when dir in @directions
-  defguardp is_valid_position(x, y) when is_number(x) and is_number(y)
+
+  defguardp is_valid_position(pos)
+            when is_tuple(pos) and
+                   tuple_size(pos) == 2 and
+                   is_number(elem(pos, 0)) and
+                   is_number(elem(pos, 1))
 
   @doc """
   Create a Robot Simulator given an initial direction and position.
@@ -13,13 +18,9 @@ defmodule RobotSimulator do
   """
   @spec create(direction :: atom, position :: {integer, integer}) :: any
   def create(dir \\ :north, pos \\ {0, 0})
-
-  def create(dir, {x, y} = pos) when is_valid_direction(dir) and is_valid_position(x, y) do
-    %__MODULE__{dir: dir, pos: pos}
-  end
-
   def create(dir, _) when not is_valid_direction(dir), do: {:error, "invalid direction"}
-  def create(_, _), do: {:error, "invalid position"}
+  def create(_, pos) when not is_valid_position(pos), do: {:error, "invalid position"}
+  def create(dir, pos), do: %__MODULE__{dir: dir, pos: pos}
 
   @doc """
   Simulate the robot's movement given a string of instructions.
@@ -29,7 +30,7 @@ defmodule RobotSimulator do
   @spec simulate(robot :: any, instructions :: String.t()) :: any
   def simulate(robot, instructions) do
     instructions
-    |> String.split("", trim: true)
+    |> String.graphemes()
     |> Enum.map(fn
       "R" -> :turn_right
       "L" -> :turn_left
@@ -40,6 +41,20 @@ defmodule RobotSimulator do
   catch
     error -> error
   end
+
+  @doc """
+  Return the robot's direction.
+
+  Valid directions are: `:north`, `:east`, `:south`, `:west`
+  """
+  @spec direction(robot :: any) :: atom
+  def direction(%__MODULE__{dir: dir}), do: dir
+
+  @doc """
+  Return the robot's position.
+  """
+  @spec position(robot :: any) :: {integer, integer}
+  def position(%__MODULE__{pos: pos}), do: pos
 
   defp execute(:turn_right, %__MODULE__{dir: dir} = robot) do
     index = @directions |> Enum.find_index(&(&1 == dir))
@@ -59,18 +74,4 @@ defmodule RobotSimulator do
       :west -> %__MODULE__{robot | pos: {x - 1, y}}
     end
   end
-
-  @doc """
-  Return the robot's direction.
-
-  Valid directions are: `:north`, `:east`, `:south`, `:west`
-  """
-  @spec direction(robot :: any) :: atom
-  def direction(%__MODULE__{dir: dir}), do: dir
-
-  @doc """
-  Return the robot's position.
-  """
-  @spec position(robot :: any) :: {integer, integer}
-  def position(%__MODULE__{pos: pos}), do: pos
 end
