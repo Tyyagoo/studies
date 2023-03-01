5dee535c1903a21dd6dc11a5ca7c8c064b55a5ea
Tyyagoo <tyyago.dev@gmail.com>
2023-03-01 13:51:03 -0300

    :sparkles: finish `elixir/rail-fence-cipher` exercise

--- exercism/elixir/rail-fence-cipher/lib/rail_fence_cipher.ex
+++ exercism/elixir/rail-fence-cipher/lib/rail_fence_cipher.ex
@@ -3,23 +3,86 @@ defmodule RailFenceCipher do
   Encode a given plaintext to the corresponding rail fence ciphertext
   """
   @spec encode(String.t(), pos_integer) :: String.t()
-  def encode(str, 1), do: str
   def encode(str, rails) do
-    do_encode(str, rails, 0, 1, %{})
-    |> Enum.map_join("", fn {_, ls} -> Enum.reverse(ls) end)
-  end
-
-  defp do_encode(<<>>, _, _, _, acc), do: acc
-  defp do_encode(str, rails, -1, _dir, acc), do: do_encode(str, rails, 1, 1, acc)
-  defp do_encode(str, limit, limit, _dir, acc), do: do_encode(str, limit, limit - 2, -1, acc)
-  defp do_encode(<<letter::utf8, rest::binary>>, rails, idx, dir, acc) do
-    do_encode(rest, rails, idx + dir, dir, Map.update(acc, idx, [letter], fn ls -> [letter | ls] end))
+    str
+    |> reduce_sink(move_through_rails(rails), %{}, &do_encode/3)
+    |> join_map_values("", &Enum.reverse/1)
   end
 
   @doc """
   Decode a given rail fence ciphertext to the corresponding plaintext
   """
   @spec decode(String.t(), pos_integer) :: String.t()
+  def decode("", _rails), do: ""
+
   def decode(str, rails) do
+    graphemes = String.graphemes(str)
+    path = move_through_rails(rails)
+    acc = 0..(rails - 1) |> Enum.map(&{&1, []}) |> Enum.into(%{})
+
+    graphemes
+    |> reduce_sink(path, acc, &do_pattern/3)
+    |> join_map_values("\n")
+    |> replace_wildcards(graphemes)
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.replace(&1, "💀", ""))
+    |> reduce_sink(path, [], &read_rails/3)
   end
+
+  defp move_through_rails(1), do: Stream.cycle([0])
+
+  defp move_through_rails(rails) do
+    down = 0..(rails - 1)
+    up = (rails - 2)..1
+
+    Stream.concat(down, up)
+    |> Stream.cycle()
+  end
+
+  defp reduce_sink(sink, path, acc, fun),
+    do: Enum.reduce_while(path, {acc, sink}, fn idx, {acc, sink} -> fun.(idx, sink, acc) end)
+
+  defp replace_wildcards(pattern, graphemes),
+    do: Enum.reduce(graphemes, pattern, &String.replace(&2, "?", &1, global: false))
+
+  defp do_encode(_, <<>>, acc), do: {:halt, acc}
+
+  defp do_encode(idx, <<x::utf8, xs::binary>>, acc) do
+    acc
+    |> Map.update(idx, [x], &[x | &1])
+    |> then(&{:cont, {&1, xs}})
+  end
+
+  defp do_pattern(_, [], acc), do: {:halt, acc}
+
+  defp do_pattern(idx, [_x | xs], acc) do
+    acc
+    |> update_all_except(idx, &["💀" | &1])
+    |> Map.update(idx, ["?"], &["?" | &1])
+    |> then(&{:cont, {&1, xs}})
+  end
+
+  defp read_rails(idx, rails, acc) do
+    case Enum.at(rails, idx) do
+      <<>> ->
+        {:halt, acc |> Enum.reverse() |> to_string()}
+
+      <<x::utf8, xs::binary>> ->
+        rails
+        |> List.update_at(idx, fn _ -> xs end)
+        |> then(&{:cont, {[x | acc], &1}})
+    end
+  end
+
+  defp update_all_except(map, exception, fun) do
+    map
+    |> Enum.map(fn
+      {^exception, _} = keep -> keep
+      {key, value} -> {key, fun.(value)}
+    end)
+    |> Enum.into(%{})
+  end
+
+  defp join_map_values(map, joiner, fun \\ & &1),
+    do: Enum.map_join(map, joiner, fn {_key, val} -> fun.(val) end)
 end
--- exercism/elixir/rail-fence-cipher/test/rail_fence_cipher_test.exs
+++ exercism/elixir/rail-fence-cipher/test/rail_fence_cipher_test.exs
@@ -38,19 +38,16 @@ defmodule RailFenceCipherTest do
   end
 
   describe "decode" do
-    @tag :pending
     test "decode with three rails" do
       cipher = "TEITELHDVLSNHDTISEIIEA"
       assert RailFenceCipher.decode(cipher, 3) == "THEDEVILISINTHEDETAILS"
     end
 
-    @tag :pending
     test "decode with five rails" do
       cipher = "EIEXMSMESAORIWSCE"
       assert RailFenceCipher.decode(cipher, 5) == "EXERCISMISAWESOME"
     end
 
-    @tag :pending
     test "decode with six rails" do
       cipher = "133714114238148966225439541018335470986172518171757571896261"
 
@@ -58,22 +55,18 @@ defmodule RailFenceCipherTest do
                "112358132134558914423337761098715972584418167651094617711286"
     end
 
-    @tag :pending
     test "decode empty string" do
       assert RailFenceCipher.decode("", 4) == ""
     end
 
-    @tag :pending
     test "decode with one rail" do
       assert RailFenceCipher.decode("ABCDEFGHIJKLMNOP", 1) == "ABCDEFGHIJKLMNOP"
     end
 
-    @tag :pending
     test "decode letters of less than rails" do
       assert RailFenceCipher.decode("More rails than letters", 24) == "More rails than letters"
     end
 
-    @tag :pending
     test "decode a more diverse text" do
       msg = "The quick brown fox jumps over the lazy dog."
       cipher = RailFenceCipher.encode(msg, 4)
