2786c8a02c181ee379f379c8fb8985db9c3520ef
Tyyagoo <tyyago.dev@gmail.com>
2023-03-20 15:30:13 -0300

    :art: make `rust/binary-search` generic

--- exercism/rust/binary-search/src/lib.rs
+++ exercism/rust/binary-search/src/lib.rs
@@ -1,16 +1,19 @@
-pub fn find(array: &[i32], key: i32) -> Option<usize> {
+use std::cmp::{Ordering, PartialOrd};
+use std::convert::AsRef;
+
+pub fn find<T: PartialOrd, U: AsRef<[T]>>(array: U, key: T) -> Option<usize> {
+    let array = array.as_ref();
+
     if array.is_empty() {
         return None;
     }
 
     let mid = (array.len() - 1) / 2;
 
-    match array.get(mid) {
-        Some(n) if *n == key => Some(mid),
-        Some(n) if *n > key => find(array.get(0..mid)?, key),
-        Some(n) if *n < key => {
-            find(array.get((mid + 1)..array.len())?, key).map(|offset| mid + offset + 1)
-        }
-        _ => None,
+    match array.get(mid)?.partial_cmp(&key) {
+        Some(Ordering::Equal) => Some(mid),
+        Some(Ordering::Greater) => find(array.get(0..mid)?, key),
+        Some(Ordering::Less) => find(array.get((mid + 1)..array.len())?, key).map(|m| mid + m + 1),
+        None => None,
     }
 }
