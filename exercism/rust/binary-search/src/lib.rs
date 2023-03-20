02d32c88259c0e820ac226b3bcd89e7375a7ca4b
Tyyagoo <tyyago.dev@gmail.com>
2023-03-20 15:48:51 -0300

    :art: `rust/binary-search` loves ?

--- exercism/rust/binary-search/src/lib.rs
+++ exercism/rust/binary-search/src/lib.rs
@@ -10,10 +10,9 @@ pub fn find<T: PartialOrd, U: AsRef<[T]>>(array: U, key: T) -> Option<usize> {
 
     let mid = (array.len() - 1) / 2;
 
-    match array.get(mid)?.partial_cmp(&key) {
-        Some(Ordering::Equal) => Some(mid),
-        Some(Ordering::Greater) => find(array.get(0..mid)?, key),
-        Some(Ordering::Less) => find(array.get((mid + 1)..array.len())?, key).map(|m| mid + m + 1),
-        None => None,
+    match array.get(mid)?.partial_cmp(&key)? {
+        Ordering::Equal => Some(mid),
+        Ordering::Greater => find(array.get(..mid)?, key),
+        Ordering::Less => find(array.get((mid + 1)..)?, key).map(|m| mid + m + 1),
     }
 }
